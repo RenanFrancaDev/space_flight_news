1c0546d5f72f3d0dbf29f8f424719785e6e76538
Renan Franca <renanfranca.dev@gmail.com>
2023-12-08 23:19:28 -0300

    feat: model Users done

---
+++ src/models/Users.js
@@ -0,0 +1,17 @@
+const mongoose = require("moongose");
+
+const UserSchema = new mongoose.UserSchema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});

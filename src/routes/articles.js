4b162d219cb50dd1eed9ad75104ff87b7b5069ea
Renan Franca <renanfranca.dev@gmail.com>
2023-12-18 21:12:10 -0300

    feat: cron configurado

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -3,32 +3,35 @@ const SchemaArticles = require("../models/Articles");
 const connectDatabase = require("../services/db");
 const router = express.Router();
 const axios = require("axios");
+const cron = require("node-cron");
 
 router.get("/", connectDatabase, async (req, res, next) => {
   const resDB = await SchemaArticles.find();
   res.status(200).json(resDB);
 });
 
-router.get("/api", async (req, res, next) => {
-  // res.status(200).json({ mgs: "OK" });
-  const resDB = await axios.get(
-    "https://api.spaceflightnewsapi.net/v4/articles/?limit=1000&offset=0"
-  );
-  const result = resDB.data.results;
-  res.status(200).json(result);
-});
+// cron.schedule("* * * * * *", () => console.log("teste cron"));
 
-router.post("/", async (req, res, next) => {
+cron.schedule(" 0 0 * * *", async () => {
   const resApi = await axios.get(
-    "https://api.spaceflightnewsapi.net/v4/articles/?limit=1000&offset=0"
+    "https://api.spaceflightnewsapi.net/v4/articles/?limit=10&offset=0"
   );
   const result = resApi.data.results;
 
   var news = await SchemaArticles.deleteMany({ id: { $gte: 1 } });
 
-  news = await SchemaArticles.create(result);
+  await SchemaArticles.create(result);
+});
 
-  res.status(201).json(news);
+router.get("/api", async (req, res, next) => {
+  // res.status(200).json({ mgs: "OK" });
+
+  const resDB = await axios.get(
+    "https://api.spaceflightnewsapi.net/v4/articles/?limit=1000&offset=0"
+  );
+  const result = resDB.data.results;
+
+  res.status(200).json(result);
 });
 
 // router.post("/", connectDatabase, async (req, res, next) => {

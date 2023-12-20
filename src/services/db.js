46eb949d05988fd2aff5d3b46246ba312cbbfae7
julio cesar <jcmcf2012@gmail.com>
2023-12-20 20:12:44 -0300

    fix: cron

--- src/services/db.js
+++ src/services/db.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const connectDatabase = async (req = null, res = null, next = null) => {
   console.log("connecting to database");
   try {
-    console.log("MONGO", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connecting database!");
     try {
